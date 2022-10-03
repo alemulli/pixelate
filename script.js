8a1c2fdf2a6e9a6f880f69cdad35cb55e16d46ad
root <root@DESKTOP-M366OAF.localdomain>
2022-10-03 10:47:11 -0400

    Lex's first turn in the driver seat

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Your code here
+const table = document.getElementById('theTable')
+const newTr = document.createElement('tr')
+
+function makeRow () {
+    table.appendChild(newTr)
+}
+
+const addRowButton = document.getElementById('add-row')
+
+addRowButton.addEventListener('click', makeRow)
