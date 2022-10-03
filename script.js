b1e363612dc2cfde4102de169faa9e7460095f04
root <root@DESKTOP-M366OAF.localdomain>
2022-10-03 11:12:49 -0400

    another lex commit

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ const newTr = document.createElement('tr')
 
 function makeRow () {
     table.appendChild(newTr)
+    console.log("function")
 }
 
+console.log("before event listener")
 const addRowButton = document.getElementById('add-row')
 
 addRowButton.addEventListener('click', makeRow)
+
+console.log("after event listener")
