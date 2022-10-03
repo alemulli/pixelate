ef8e65959ee3fa350c6bef7dd288189fca2a3b81
root <root@DESKTOP-M366OAF.localdomain>
2022-10-03 12:08:33 -0400

    color toggling

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 // Your code here
-const table = document.getElementById('theTable')
+const table = document.getElementById("theTable");
 
-function makeRow () {
-    const newTr = document.createElement('tr')
-    for (let i = 0; i < 20; i++) {
-        const td = document.createElement('td')
-        newTr.appendChild(td)
-    }
-    table.appendChild(newTr)
-    
+function makeRow() {
+  const newTr = document.createElement("tr");
+  for (let i = 0; i < 20; i++) {
+    const td = document.createElement("td");
+    newTr.appendChild(td);
+  }
+  table.appendChild(newTr);
 }
 
+const addRowButton = document.getElementById("add-row");
 
-const addRowButton = document.getElementById('add-row')
-
-addRowButton.addEventListener('click', makeRow)
+addRowButton.addEventListener("click", makeRow);
 
 function colorize(clickEvent) {
-if (clickEvent.target.matches("td")) {
-    console.log("clicked!")
-}
+  const target = clickEvent.target;
+  if (clickEvent.target.matches("td")) {
+    if (target.className.length) {
+      target.className = "";
+    } else {
+      clickEvent.target.className = "red";
+    }
+  }
 }
 
-table.addEventListener('click', colorize)
+table.addEventListener("click", colorize);
